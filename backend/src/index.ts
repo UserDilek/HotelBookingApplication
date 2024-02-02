13ae9a5e6ba76a015a8e967be69a95abae1dbc57
UserDilek <dilekhocaoglu11@hotmail.com>
2024-02-01 23:18:39 -0500

    Backend + frontend setup completed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,19 @@
-import express from 'express';
+import express ,{Request,Response}from 'express';
+import cors from 'cors';
+import "dotenv/config";
+
+
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
 
-app.get('/api/test', function (req, res) {
-    res.json('It is first api example')
-  });
+
+app.get("/api/test",async(req:Request,res:Response)=>{
+  res.json({message:"This is first api example"});
+})
 
 
 app.listen(7000, ()=>{
