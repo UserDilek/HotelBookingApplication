63af9fff8c082bf5954daf3bc642c91c0cc13a64
Iamjdz <jdincanada2023@gmail.com>
2024-03-05 14:49:07 -0500

    delete api call

--- frontend/src/pages/EditHotel.tsx
+++ frontend/src/pages/EditHotel.tsx
@@ -1,12 +1,23 @@
 import HotelForm from "../components/HotelForm";
+import axios from 'axios';
 
 const EditHotel = () => {
     const handleSubmit = (data:any) => {
         console.log(data);
     };
 
+    const handleDelete = (hotelId: string) => {
+        axios.post("/delete", { _id: hotelId })
+            .then(res => {
+                console.log(res.data.message);
+            })
+            .catch(error => {
+                console.log(error.response.data.Message);
+            });
+    };
+
     return (
-        <HotelForm label="Edit Hotel" handleFormSubmit={handleSubmit} />
+        <HotelForm label="Edit Hotel" handleFormSubmit={handleSubmit} handleDelete={handleDelete}/>
     )
 }
 
